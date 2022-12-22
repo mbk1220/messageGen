b364e2911aad209a725a496e9cee098ed6cb63c4
mbk1220 <millerboy7371@gmail.com>
2022-12-22 15:54:46 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var message = "Hi there! ";
+var wordBank = ["What are you doing today?", "What are you doing tomorrow?", "What did you do yesterday?", "Did you have fun?", "When are you doing something fun?"];
+
+for (var i = 0; i < wordsBank.length; i++) {
+    message += " " + wordsBank[i];
+}
