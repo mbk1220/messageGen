338ff055a081d45f2c520eb90cd72e8132af570d
mbk1220 <millerboy7371@gmail.com>
2022-12-22 15:59:11 -0600

    added random message to console

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var message = "Hi there! ";
 var wordBank = ["What are you doing today?", "What are you doing tomorrow?", "What did you do yesterday?", "Did you have fun?", "When are you doing something fun?"];
 
-for (var i = 0; i < wordsBank.length; i++) {
-    message += " " + wordsBank[i];
-}
+var randomNumber = Math.floor(Math.random() * wordBank.length);
+
+console.log(message + wordBank[randomNumber]);
